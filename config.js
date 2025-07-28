592f652b4d48462987b5bea8271c147977acfe88
Shaliya56 <161951978+Shaliya56@users.noreply.github.com>
2025-07-28 20:45:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "7UciXZ4R#bFpA9cXuiXZ165wNONmBvp_S-wBESnOzGJ80ZezMC2Y",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Shaliya56/SHALI-MD/blob/950d82b580993255742d3df40aa59beeebbc5d37/Images/20250727_133740.png",
+ALIVE_IMG: process.env.ALIVE_IMG || "",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 SHALI-MD Is Alive Now😍*",
 BOT_OWNER: '94761167172',  // Replace with the owner's phone number
 
