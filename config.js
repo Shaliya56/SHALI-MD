ab54f72e8370f689a1d7f15387a3c04124040033
Shaliya56 <161951978+Shaliya56@users.noreply.github.com>
2025-07-28 22:22:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "7UciXZ4R#bFpA9cXuiXZ165wNONmBvp_S-wBESnOzGJ80ZezMC2Y",
-ALIVE_IMG: process.env.ALIVE_IMG || "",
+ALIVE_IMG: process.env.ALIVE_IMG || "`https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true`",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 SHALI-MD Is Alive Now😍*",
 BOT_OWNER: '94761167172',  // Replace with the owner's phone number
 
